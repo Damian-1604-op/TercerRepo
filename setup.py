b4d87911902b4039f90fbd7e3a9d2011a5c6405f
Damian Salazar Arevalo <damiansa777x@gmail.com>
2026-03-11 09:58:49 -0600

    Nuevo lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Damian Salazar",                         # Tu nombre
+    author_email="damiansa777x@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Damian-1604-op/TercerRepo#",     # URL del proyecto
+)
